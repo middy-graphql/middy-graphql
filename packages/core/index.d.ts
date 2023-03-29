ccc82e2364c7abdeef5a0ac4371960b76fea1a3d
Kyuhak Yuk <kyuhakyuk@kyuhaks-mbp.mynetworksettings.com>
2023-03-29 13:46:20 -0700

    fix: fix context type

--- packages/core/index.d.ts
+++ packages/core/index.d.ts
@@ -1,41 +1,58 @@
-import { GraphQLError, GraphQLResolveInfo } from "graphql";
+import { GraphQLError, GraphQLResolveInfo } from 'graphql'
+import { Request as ExpressRequest, Response as ExpressResponse } from 'express'
 declare function middy(baseResolver: Resolver): {
-  (root: any, args: any, context: any, info: GraphQLResolveInfo): Promise<any>;
-  use(middlewares: MiddlewareObj | MiddlewareObj[]): any;
-  applyMiddleware(middleware?: MiddlewareObj): any;
-  before(beforeMiddleware: MiddlewareFn): any;
-  after(afterMiddleware: MiddlewareFn): any;
-  onError(onErrorMiddleware: MiddlewareFn): any;
+  (root: any, args: any, context: any, info: GraphQLResolveInfo): Promise<any>
+  use(middlewares: MiddlewareObj | MiddlewareObj[]): any
+  applyMiddleware(middleware?: MiddlewareObj): any
+  before(beforeMiddleware: MiddlewareFn): any
+  after(afterMiddleware: MiddlewareFn): any
+  onError(onErrorMiddleware: MiddlewareFn): any
   __middlewares: {
-    before: MiddlewareFn<any, any, any, any>[];
-    after: MiddlewareFn<any, any, any, any>[];
-    onError: MiddlewareFn<any, any, any, any>[];
-  };
-};
+    before: MiddlewareFn<any, any, any, any>[]
+    after: MiddlewareFn<any, any, any, any>[]
+    onError: MiddlewareFn<any, any, any, any>[]
+  }
+}
+export declare interface Context {
+  [key: string]: any
+  req: ExpressRequest
+  res: ExpressResponse
+}
 export declare type Request<
   TRoot = any,
   TArgs = any,
-  ContextType = any,
+  ContextType = Context,
   TResult = any
 > = ResolverData<TRoot, TArgs, ContextType> & {
-  response?: TResult;
-  error?: GraphQLError;
-};
-export declare type MiddlewareFn<TRoot = any, TArgs = any, TContext = any, TResult = any> = (
-  data: Request<TRoot, TArgs, TContext, TResult>
-) => TResult;
-export interface MiddlewareObj<TRoot = any, TArgs = any, TContext = any, TResult = any> {
-  before?: MiddlewareFn<TRoot, TArgs, TContext, TResult>;
-  after?: MiddlewareFn<TRoot, TArgs, TContext, TResult>;
-  onError?: MiddlewareFn<TRoot, TArgs, TContext, TResult>;
+  response?: TResult
+  error?: GraphQLError
 }
-export declare type Resolver<TRoot = any, TArgs = any, TContext = any, TResult = any> = (
-  data: ResolverData<TRoot, TArgs, TContext>
-) => TResult;
-export interface ResolverData<TRoot, TArgs, TContext> {
-  root: TRoot;
-  args: TArgs;
-  context: TContext;
-  info: GraphQLResolveInfo;
+export declare type MiddlewareFn<
+  TRoot = any,
+  TArgs = any,
+  TContext = Context,
+  TResult = any
+> = (data: Request<TRoot, TArgs, TContext, TResult>) => TResult
+export interface MiddlewareObj<
+  TRoot = any,
+  TArgs = any,
+  TContext = Context,
+  TResult = any
+> {
+  before?: MiddlewareFn<TRoot, TArgs, TContext, TResult>
+  after?: MiddlewareFn<TRoot, TArgs, TContext, TResult>
+  onError?: MiddlewareFn<TRoot, TArgs, TContext, TResult>
 }
-export default middy;
+export declare type Resolver<
+  TRoot = any,
+  TArgs = any,
+  TContext = Context,
+  TResult = any
+> = (data: ResolverData<TRoot, TArgs, TContext>) => TResult
+export interface ResolverData<TRoot, TArgs, TContext = Context> {
+  root: TRoot
+  args: TArgs
+  context: TContext
+  info: GraphQLResolveInfo
+}
+export default middy
